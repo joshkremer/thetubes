3e40d26bed5d87a1db0e444f79c165f672603bf6
BOSSFACE <ejkcat1@gmail.com>
2017-06-12 20:22:24 -0400

    API FILES
    Created thetubes.js, got code for that...
    remade index.php, actually does something now!

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="thetubes.js" type="text/javascript"></script>
+        <script src="https://apis.google.com/js/client.js?onload=onClientLoad" type="text/javascript"></script>
+    </head>
+    <body>
+        <pre id="response"></pre>
+        <h1>test</h1>
+    </body>
 
-echo "the tubes"
-
-?>
+</html>
